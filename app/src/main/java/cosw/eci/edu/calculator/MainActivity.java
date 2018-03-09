6827d9fbdf849b02d93d94692a43085b911a33f5
Carlos Andres Sanchez <carlos.sanchez-v@mail.escuelaing.edu.co>
2018-03-09 11:54:41 -0500

    final commit

--- app/src/main/java/cosw/eci/edu/calculator/MainActivity.java
+++ app/src/main/java/cosw/eci/edu/calculator/MainActivity.java
@@ -14,14 +14,15 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
     private TextView stackTv;
-    private String currentText;
+    private TextView logText;
     private Stack<String> stack;
+    private CalculatorLogic cl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        currentText = "";
-        Stack<String> stack=new Stack<>();
+        stack=new Stack<>();
         setContentView(R.layout.activity_main);
+        cl=new CalculatorLogic();
         init();
     }
 
@@ -29,43 +30,84 @@ public class MainActivity extends AppCompatActivity {
 
         tv = (TextView) findViewById(R.id.textView);
         stackTv = (TextView) findViewById(R.id.stack);
+        logText = (TextView) findViewById(R.id.logText);
+        //second requeriment, the other listeners are on the xml view
+        Button addToStackButton = (Button) findViewById(R.id.addToStack);
+        addToStackButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                onClickAddToStack(v);
+            }
+        });
     }
 
 
-    protected void setText(String text){
-        currentText=text;
-        tv.setText(currentText);
+    protected void setTextView(String text){
+
+        tv.setText(text);
 
     }
 
     protected void onClickNumber(View v){
         Button pressed = (Button) v;
 
-        setText(currentText + pressed.getText().toString());
+        setTextView(tv.getText() + pressed.getText().toString());
 
     }
 
     protected void onClickOperator(View v){
         Button pressed = (Button) v;
 
-        setText(currentText + pressed.getText().toString());
+        setTextView(tv.getText() + pressed.getText().toString());
     }
 
     protected void onClickCleanTextView(View v){
-
-        setText("");
+        stack = new Stack<>();
+        setStackView();
+        setTextView("");
+        logText.setText("");
     }
 
     protected void onClickEquals(View v){
+            try{
+                Double res= cl.calculate(stack);
+                stack=new Stack<>();
+                stack.push(res.toString());
+                setStackView();
+            }catch(Exception e){
 
-            //setText(CalculatorLogic.calculate(logicalText)+"");
+            }
 
     }
     protected void onClickAddToStack(View v){
+        Button pressed = (Button) v;
+        System.out.println("En listener "+tv.getText().toString());
+        System.out.println(cl.isNumberOrOperator(tv.getText().toString()));
+        if(cl.isNumberOrOperator(tv.getText().toString())){
+            stack.push(tv.getText().toString());
+            setStackView();
+            setTextView("");
+        }else{
+            logText.setText("Cant add to the stack");
+        }
+    }
 
+    protected void setStackView(){
+        String res = "";
+        for (Object elem:stack.toArray()){
+            res=elem.toString()+"\n"+res;
+        }
+        stackTv.setText(res);
     }
 
     protected void onClickPoint(View v){
+        onClickNumber(v);
+    }
 
+    protected void onClickChangeNumber(View v){
+        try {
+            tv.setText(cl.tryChangeNumber(tv.getText().toString()));
+        } catch (Exception e) {
+            logText.setText("Cant change then number");
+        }
     }
 }
--- app/src/main/java/cosw/eci/edu/calculator/model/CalculatorLogic.java
+++ app/src/main/java/cosw/eci/edu/calculator/model/CalculatorLogic.java
@@ -8,32 +8,43 @@ import java.util.Stack;
 
 public class CalculatorLogic {
 
-    public static double calculate(String text) throws Exception {
-        String[] lst = text.split("jump");
-        for (int i = 0; i < lst.length; i++) {
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!! "+lst[i]);
-        }
-        Stack<String> tks = new Stack<>();
-        tks.push("3");
-        tks.push("4");
-        tks.push("+");
-        System.out.println("Resolviendo ando"+ calculate(tks));
-        return 0.0;
+    public double calculate(Stack<String> tks) throws Exception {
+        return calculateInternal(tks);
     }
 
 
-    private static double calculate(Stack<String> tks) throws Exception {
+    private double calculateInternal(Stack<String> tks) throws Exception {
         String tk = tks.pop();
-        double x,y;
-        try {x = Double.parseDouble(tk);}
-        catch (Exception e) {
-            y = calculate(tks);  x = calculate(tks);
-            if      (tk.equals("+"))  x += y;
-            else if (tk.equals("-"))  x -= y;
-            else if (tk.equals("*"))  x *= y;
-            else if (tk.equals("/"))  x /= y;
+        double x, y;
+        try {
+            x = Double.parseDouble(tk);
+        } catch (Exception e) {
+            y = calculateInternal(tks);
+            x = calculateInternal(tks);
+            if (tk.equals("+")) x += y;
+            else if (tk.equals("-")) x -= y;
+            else if (tk.equals("*")) x *= y;
+            else if (tk.equals("/")) x /= y;
             else throw new Exception();
         }
         return x;
+    }
+
+    public Boolean isNumberOrOperator(String toEval) {
+        Boolean ans = true;
+        try {
+            Double x = Double.parseDouble(toEval);
+        } catch (Exception e) {
+            ans = (toEval.equals("+") || toEval.equals("-") || toEval.equals("*") || toEval.equals("/"));
+        }
+        return ans;
+    }
+
+    public String tryChangeNumber(String num) throws Exception {
+        String ans = num;
+        Double x = Double.parseDouble(num);
+        if (x>0) ans="-"+ans;
+        else if (x<0) ans=ans.substring(1);
+        return ans;
     }
 }
